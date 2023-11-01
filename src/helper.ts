5d5741581622423419aa040597d8abf6f1654cb7
eivmosn <eivmosn@163.com>
2023-11-01 23:12:35 +0800

    feat: add makeMap function

--- src/helper.ts
+++ src/helper.ts
@@ -112,3 +112,17 @@ export function set(object: MaybeObject, path: string, value: any): void {
   }
   object[paths[paths.length - 1]] = value
 }
+
+/**
+ * @vue-shared
+ */
+export function makeMap(
+  str: string,
+  expectsLowerCase?: boolean,
+): (key: string) => boolean {
+  const map: Record<string, boolean> = Object.create(null)
+  const list: Array<string> = str.split(',')
+  for (let i = 0; i < list.length; i++)
+    map[list[i]] = true
+  return expectsLowerCase ? val => !!map[val.toLowerCase()] : val => !!map[val]
+}
