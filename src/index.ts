f9bdab2b58a84fbe4c7e60e86eab24c8cf2a0dbe
eivmosn <eivmosn@163.com>
2023-12-23 23:18:16 +0800

    fix: compile function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
 export * from './type'
+
+export function compile(code: string) {
+  code = `with (sandbox) { ${code} }`
+  const fn = new Function('sandbox', code)
+  return (sandbox: any) => {
+    const proxy = new Proxy(sandbox, {
+      has() {
+        return true
+      },
+      get(target, key, receiver) {
+        if (key === Symbol.unscopables)
+          return undefined
+        return Reflect.get(target, key, receiver)
+      },
+    })
+    return fn(proxy)
+  }
+}
+
+export function createScript(code: string, sandbox: any = {}) {
+  const excute = compile(code)
+  return excute({
+    ...sandbox,
+    JSON,
+    console,
+    Object,
+    Array,
+  })
+}
