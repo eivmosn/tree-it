27d56b35dfc34700b9024a40e319fc1660b214ee
eivmosn <eivmosn@163.com>
2024-01-26 18:31:23 +0800

    fix: add blob to string function

--- src/index.ts
+++ src/index.ts
@@ -38,3 +38,15 @@ export function fromString(style: string) {
     return acc
   }, {})
 }
+
+export function blobToBase64(blob: Blob) {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(blob)
+    reader.onload = () => {
+      const base64 = reader.result as string
+      resolve(base64)
+    }
+    reader.onerror = error => reject(error)
+  })
+}
