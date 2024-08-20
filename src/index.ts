b9f773907abd984a317e43bab057e6a239afc404
eivmosn <eivmosn@163.com>
2024-08-20 15:35:54 +0800

    feat: update eslint v9

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export const hello = 'world'
+export function get<T extends object>(source: T, path: string, defaultValue = undefined) {
+  const keys = path.replace(/\[(\d+)\]/g, '.$1').split('.')
+  const result = keys.reduce((obj, key) => {
+    return Object(obj)[key]
+  }, source)
+  return result === undefined ? defaultValue : result
+}
+
+export function set<T extends object>(source: T, path: string, value: any) {
+  const keys = path.replace(/\[(\d+)\]/g, '.$1').split('.')
+  const lastKey = keys.pop()
+  const target = keys.reduce((obj, key) => {
+    if (!Object.prototype.hasOwnProperty.call(obj, key))
+      Object(obj)[key] = {}
+    return Object(obj)[key]
+  }, source)
+  if (lastKey)
+    target[lastKey as keyof T] = value
+}
